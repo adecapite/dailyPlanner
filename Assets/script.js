9d94d62e53a9d36629314ae6078dd33c56d574c2
Anthony DeCapite <anthony.decapite@icloud.com>
2021-02-24 21:09:45 -0500

    Stopping for the night, Created variables for hour blocks in JS. Need define inputs in localstorage.

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,28 @@
 var jumboTron = document.getElementById("jumbotron")
 var timeAndDateEl = $('#clock')
+var  saveButtonEl = $('.saveButton')
 
 function showTime() {
   var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
   timeAndDateEl.text(currentTime);
 }
 setInterval(showTime, 1000);
+
+saveButtonEl.on("click", function() {
+    var input = $(this).siblings("textarea").val();
+    var storedInputs = $(this).parent().attr("id");
+    localStorage.setItem("storedInputs", JSON.stringify(input));
+}
+);
+
+
+$("#9 textarea").val(JSON.parse(localStorage.getItem('input'))),
+$("#10 textarea").val(JSON.parse(localStorage.getItem('input'))),
+$("#11 textarea").val(JSON.parse(localStorage.getItem('input'))),
+$("#12 textarea").val(JSON.parse(localStorage.getItem('input'))),
+$("#13 textarea").val(JSON.parse(localStorage.getItem('input'))),
+$("#14 textarea").val(JSON.parse(localStorage.getItem('input'))),
+$("#15 textarea").val(JSON.parse(localStorage.getItem('input'))),
+$("#16 textarea").val(JSON.parse(localStorage.getItem('input'))),
+$("#17 textarea").val(JSON.parse(localStorage.getItem('input')))
+
