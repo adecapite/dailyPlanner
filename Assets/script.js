d19e407950308e0a0d98a43cc11855bea5c746bd
Anthony DeCapite <anthony.decapite@icloud.com>
2021-02-26 16:16:19 -0500

    added functions for past present and future.

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,8 @@
 var jumboTron = document.getElementById("jumbotron")
 var timeAndDateEl = $('#clock')
 var  saveButtonEl = $('.saveButton')
+var currentHour = moment.format('H')
+
 
 function showTime() {
   var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
@@ -12,17 +14,29 @@ saveButtonEl.on("click", function() {
     var input = $(this).siblings("textarea").val();
     var storedInputs = $(this).parent().attr("id");
     localStorage.setItem("storedInputs", JSON.stringify(input));
+    document.createElement("li").appendChild(input)
 }
 );
 
+$("#9 ").val(JSON.parse(localStorage.getItem('input'))),
+$("#10 ").val(JSON.parse(localStorage.getItem('input'))),
+$("#11").val(JSON.parse(localStorage.getItem('input'))),
+$("#12").val(JSON.parse(localStorage.getItem('input'))),
+$("#13").val(JSON.parse(localStorage.getItem('input'))),
+$("#14").val(JSON.parse(localStorage.getItem('input'))),
+$("#15").val(JSON.parse(localStorage.getItem('input'))),
+$("#16").val(JSON.parse(localStorage.getItem('input'))),
+$("#17").val(JSON.parse(localStorage.getItem('input')))
 
-$("#9 textarea").val(JSON.parse(localStorage.getItem('input'))),
-$("#10 textarea").val(JSON.parse(localStorage.getItem('input'))),
-$("#11 textarea").val(JSON.parse(localStorage.getItem('input'))),
-$("#12 textarea").val(JSON.parse(localStorage.getItem('input'))),
-$("#13 textarea").val(JSON.parse(localStorage.getItem('input'))),
-$("#14 textarea").val(JSON.parse(localStorage.getItem('input'))),
-$("#15 textarea").val(JSON.parse(localStorage.getItem('input'))),
-$("#16 textarea").val(JSON.parse(localStorage.getItem('input'))),
-$("#17 textarea").val(JSON.parse(localStorage.getItem('input')))
+
+function presentTime(){
+  Element.classList.add('present')
+}
+function pastTime(){
+  Element.classList.add('past')
+}
+
+function futureTime{
+  Element.classList.add('future')
+}
 
